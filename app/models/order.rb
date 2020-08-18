8123eab26ad85d5d1456b8599c7ff8b26512718c
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-18 11:40:40 -0500

    added to confirm_order functionality

--- app/models/order.rb
+++ app/models/order.rb
@@ -24,13 +24,25 @@ class Order < ActiveRecord::Base
 
     def self.order_confirm(drink_instance)
         if User.is_signed_in 
-            Order.create({username: User.find_by(signed_in?: true)?
-            true : false})
+            Order.create({user_id: User.find_by(signed_in?: true).id, drink_id: drink_instance.id, price: drink_instance.price})
+            Order.last.favorite
+        else
+            Order.create({drink_id: drink_instance.id, price: drink_instance.price})
+        end
         system "clear"
         puts "You have successfully ordered a #{drink_instance.name}. Thank you for your patronage."
         sleep (3)
         welcome
     end
+
+    def favorite
+        prompt = TTY::Prompt.new
+        prompt.select("Would you like to add this order to your Favorites?") do |menu|
+            menu.choice "Yes", -> {self.update(favorite?: true)}
+            menu.choice "No", -> {"Order not added to your Favorites"}
+        end
+    end
+
 
 
 
