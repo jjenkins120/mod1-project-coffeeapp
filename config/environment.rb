ccb9467470af8f63f5ddd0f395b112574ee72c7d
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-18 09:41:24 -0500

    testing sql deletion code

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,8 @@ require "tty-prompt"
 
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-ActiveRecord::Base.logger.level = 1
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
 require_all 'app'
 
 
