a7f2fc37b7270a3b0419bb75c83200bf9afa07a9
jjenkins120 <jjenkins120@gmail.com>
2020-08-20 10:43:19 -0500

    sound files added

---
+++ app/models/graphics_and_sounds.rb
@@ -0,0 +1,230 @@
+#sound and music variables
+$pouring_coffee_sound = Sound.new("pouring_coffee.mp3")
+$ding_sound_effect = Sound.new("ding.mp3")
+$pop_sound_effect = Sound.new("pop.mp3")
+$background_music = Music.new("background_jazz.wav")
+$background_music.volume = 10
+
+#Graphic Methods
+def opening_graphic_first_color
+system "clear"
+puts "
+                                      :`                                
+                                         .h                                
+                                         :m-                               
+                                    `   `hd`                               
+                                   --  `hd-                                
+                                   s-  /m-                                 
+                                   h/  :y                                  
+                                   sd.  +`                                 
+                                   `hd:                                    
+                                    `+dy-                                  
+                                      .od+                                 
+                                        :ho                                
+                                         .d.                               
+                                          o-                               
+                                          :`                               
+                            ``............-.............``                 
+                    `.---:---.````````           ```````..--:---.`         
+                `-//:-.``                                     `..://:.     
+              :so-`                                                `./s/`  
+ ```````````.sd:           `.-://+ossyyyyyhhhyysssooo+/:-..`          .yh- 
+shhhhhhhhhhhhmd`      .:+shhddmmmmmmmmmmmmmmmmddhysoo+ooosssyo/-`      smh 
+hmmmmhhhhhhhdmmh:`  :ydmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddhyo/:-+ydh+`  -ommd 
++mmmh```````-mmmmho/mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdy/`hmmo+ydmmms 
+`hmmm.       dmh/shdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmymmmmhy+smm/ 
+ .dmmy`      +mm. `.:+syhddmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddhhso/-.  hmd` 
+  .ymms.     `dmo       `..-:/+oosyyyhhhhhhhhhhhhyyysso+/:--.`       /mmo  
+   `+hmh/`    :mm-                    ``````````                    .dmd`  
+     `/ydh+.   omh.                                                `ymd:   
+        -/syo/..smy`                                              `smm/    
+            .---.+dh.                                            `smd/     
+                  :hd:                                          .ymh-      
+            ``     `odo.                                      `+ddo`     ` 
+           `:        .sh+`                                  `:hds-       `-
+           /-          .+y+.`                             `/yho.          s
+           o+            `-oo/.`                      `.:ohs:`           .d
+           :d:            `` .//+/-```          ``.-/+ss/-``            `yy
+            sd+`           -:.`  `-:::://///+++++++:-. `.::`           :hh.
+             +dd+.           -///:-````        ````.-///:`          `:yds` 
+              `+ddy/.`           .-/////+++++++////:.`           `:odds-   
+                `:oddhs/-`                                  `.:oyddy/.     
+                    -/shddhso/:-```                 ``.-/+sydddy+-`        
+                        `-/+syddmddhyyysssssssssyyhhdmddhso/:. ".colorize(:white)
+puts "
+
+              ██╗  ██╗   ██╗        ██╗     ██████╗ █████╗ ███████╗███████╗
+              ██║ ██╔╝   ██║        ██║    ██╔════╝██╔══██╗██╔════╝██╔════╝
+              █████╔╝ ████████╗     ██║    ██║     ███████║█████╗  █████╗  
+              ██╔═██╗ ██╔═██╔═╝██   ██║    ██║     ██╔══██║██╔══╝  ██╔══╝  
+              ██║  ██╗██████║  ╚█████╔╝    ╚██████╗██║  ██║██║     ███████╗
+              ╚═╝  ╚═╝╚═════╝   ╚════╝      ╚═════╝╚═╝  ╚═╝╚═╝     ╚══════╝
+".colorize(:green)
+sleep(0.4)
+end
+
+def opening_graphic_second_color
+system "clear"
+puts "
+                                          :`                                
+                                      --  `hd-                                
+                                      s-  /m-                                 
+                                      h/  :y                                  
+                                      sd.  +`                                 
+                                      `hd:                                        
+                                      s-  /m-                                 
+                                      h/  :y                                  
+                                      sd.  +`                                 
+                                      `hd:                                    
+                                        `+dy-                                  
+                                        /m-                                 
+                                      h/  :y                                  
+                                      sd.  +`  
+                                                                                             
+                            ``............-.............``                 
+                    `.---:---.````````           ```````..--:---.`         
+                `-//:-.``                                     `..://:.     
+              :so-`                                                `./s/`  
+ ```````````.sd:           `.-://+ossyyyyyhhhyysssooo+/:-..`          .yh- 
+shhhhhhhhhhhhmd`      .:+shhddmmmmmmmmmmmmmmmmddhysoo+ooosssyo/-`      smh 
+hmmmmhhhhhhhdmmh:`  :ydmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddhyo/:-+ydh+`  -ommd 
++mmmh```````-mmmmho/mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdy/`hmmo+ydmmms 
+`hmmm.       dmh/shdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmymmmmhy+smm/ 
+ .dmmy`      +mm. `.:+syhddmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddhhso/-.  hmd` 
+  .ymms.     `dmo       `..-:/+oosyyyhhhhhhhhhhhhyyysso+/:--.`       /mmo  
+   `+hmh/`    :mm-                    ``````````                    .dmd`  
+     `/ydh+.   omh.                                                `ymd:   
+        -/syo/..smy`                                              `smm/    
+            .---.+dh.                                            `smd/     
+                  :hd:                                          .ymh-      
+            ``     `odo.                                      `+ddo`     ` 
+           `:        .sh+`                                  `:hds-       `-
+           /-          .+y+.`                             `/yho.          s
+           o+            `-oo/.`                      `.:ohs:`           .d
+           :d:            `` .//+/-```          ``.-/+ss/-``            `yy
+            sd+`           -:.`  `-:::://///+++++++:-. `.::`           :hh.
+             +dd+.           -///:-````        ````.-///:`          `:yds` 
+              `+ddy/.`           .-/////+++++++////:.`           `:odds-   
+                `:oddhs/-`                                  `.:oyddy/.     
+                    -/shddhso/:-```                 ``.-/+sydddy+-`        
+                        `-/+syddmddhyyysssssssssyyhhdmddhso/:. ".colorize(:white)
+ puts "
+
+              ██╗  ██╗   ██╗        ██╗     ██████╗ █████╗ ███████╗███████╗
+              ██║ ██╔╝   ██║        ██║    ██╔════╝██╔══██╗██╔════╝██╔════╝
+              █████╔╝ ████████╗     ██║    ██║     ███████║█████╗  █████╗  
+              ██╔═██╗ ██╔═██╔═╝██   ██║    ██║     ██╔══██║██╔══╝  ██╔══╝  
+              ██║  ██╗██████║  ╚█████╔╝    ╚██████╗██║  ██║██║     ███████╗
+              ╚═╝  ╚═╝╚═════╝   ╚════╝      ╚═════╝╚═╝  ╚═╝╚═╝     ╚══════╝
+".colorize(:white)  
+sleep(0.4)
+system "clear"
+end 
+
+def coffee_graphic
+puts "
+                                      :`                                
+                                         .h                                
+                                         :m-                               
+                                    `   `hd`                               
+                                   --  `hd-                                
+                                   s-  /m-                                 
+                                   h/  :y                                  
+                                   sd.  +`                                 
+                                   `hd:                                    
+                                    `+dy-                                  
+                                      .od+                                 
+                                        :ho                                
+                                         .d.                               
+                                          o-                               
+                                          :`                               
+                            ``............-.............``                 
+                    `.---:---.````````           ```````..--:---.`         
+                `-//:-.``                                     `..://:.     
+              :so-`                                                `./s/`  
+ ```````````.sd:           `.-://+ossyyyyyhhhyysssooo+/:-..`          .yh- 
+shhhhhhhhhhhhmd`      .:+shhddmmmmmmmmmmmmmmmmddhysoo+ooosssyo/-`      smh 
+hmmmmhhhhhhhdmmh:`  :ydmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddhyo/:-+ydh+`  -ommd 
++mmmh```````-mmmmho/mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdy/`hmmo+ydmmms 
+`hmmm.       dmh/shdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmymmmmhy+smm/ 
+ .dmmy`      +mm. `.:+syhddmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddhhso/-.  hmd` 
+  .ymms.     `dmo       `..-:/+oosyyyhhhhhhhhhhhhyyysso+/:--.`       /mmo  
+   `+hmh/`    :mm-                    ``````````                    .dmd`  
+     `/ydh+.   omh.                                                `ymd:   
+        -/syo/..smy`                                              `smm/    
+            .---.+dh.                                            `smd/     
+                  :hd:                                          .ymh-      
+            ``     `odo.                                      `+ddo`     ` 
+           `:        .sh+`                                  `:hds-       `-
+           /-          .+y+.`                             `/yho.          s
+           o+            `-oo/.`                      `.:ohs:`           .d
+           :d:            `` .//+/-```          ``.-/+ss/-``            `yy
+            sd+`           -:.`  `-:::://///+++++++:-. `.::`           :hh.
+             +dd+.           -///:-````        ````.-///:`          `:yds` 
+              `+ddy/.`           .-/////+++++++////:.`           `:odds-   
+                `:oddhs/-`                                  `.:oyddy/.     
+                    -/shddhso/:-```                 ``.-/+sydddy+-`        
+                        `-/+syddmddhyyysssssssssyyhhdmddhso/:. ".colorize(:white)
+end
+
+def opening_graphic
+  $pouring_coffee_sound.play
+  7.times do
+  opening_graphic_first_color
+  opening_graphic_second_color
+  end
+  opening_graphic_first_color
+  sleep(1.25)
+  $ding_sound_effect.play
+  sleep(0.25)
+  puts "\n\t\t\t\tKimberlyn and Jeff ©2020"
+  sleep(6)
+end
+
+def welcome_graphic
+puts"
+██     ██ ███████ ██       ██████  ██████  ███    ███ ███████ ██ 
+██     ██ ██      ██      ██      ██    ██ ████  ████ ██      ██ 
+██  █  ██ █████   ██      ██      ██    ██ ██ ████ ██ █████   ██ 
+██ ███ ██ ██      ██      ██      ██    ██ ██  ██  ██ ██         
+ ███ ███  ███████ ███████  ██████  ██████  ██      ██ ███████ ██\n".colorize(:green)
+end
+
+def order_graphic
+puts"
+ ██████  ██████  ██████  ███████ ██████  
+██    ██ ██   ██ ██   ██ ██      ██   ██ 
+██    ██ ██████  ██   ██ █████   ██████  
+██    ██ ██   ██ ██   ██ ██      ██   ██ 
+ ██████  ██   ██ ██████  ███████ ██   ██\n".colorize(:green)
+end
+
+def checkout_graphic
+puts"
+ ██████ ██   ██ ███████  ██████ ██   ██  ██████  ██    ██ ████████ 
+██      ██   ██ ██      ██      ██  ██  ██    ██ ██    ██    ██    
+██      ███████ █████   ██      █████   ██    ██ ██    ██    ██    
+██      ██   ██ ██      ██      ██  ██  ██    ██ ██    ██    ██    
+ ██████ ██   ██ ███████  ██████ ██   ██  ██████   ██████     ██ \n".colorize(:green)
+end
+
+def nailed_it_graphic
+puts"
+███    ██  █████  ██ ██      ███████ ██████      ██ ████████ 
+████   ██ ██   ██ ██ ██      ██      ██   ██     ██    ██    
+██ ██  ██ ███████ ██ ██      █████   ██   ██     ██    ██    
+██  ██ ██ ██   ██ ██ ██      ██      ██   ██     ██    ██    
+██   ████ ██   ██ ██ ███████ ███████ ██████      ██    ██ ██ \n".colorize(:green)
+end
+
+def goodbye_graphic
+puts "
+
+  ██████╗  ██████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███████╗██╗
+ ██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝██║
+ ██║  ███╗██║   ██║██║   ██║██║  ██║██████╔╝ ╚████╔╝ █████╗  ██║
+ ██║   ██║██║   ██║██║   ██║██║  ██║██╔══██╗  ╚██╔╝  ██╔══╝  ╚═╝
+ ╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝██████╔╝   ██║   ███████╗██╗
+  ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝
+  ".colorize(:green)
+end
